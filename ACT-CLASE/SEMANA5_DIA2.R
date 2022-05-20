fcd292c05b45c0d74fb005cb89875ef58778738c
santiago <santiago_forestal@hotmail.com>
2022-05-20 15:04:17 -0500

    #ACTUALIZADO

---
+++ ACT-CLASE/SEMANA5_DIA2.R
@@ -0,0 +1,21 @@
+# SANTIAGO
+# Semana 5
+# 17/02/2022
+
+#Estadística diferencial
+
+# pruebas dT (una muestra, muestra dependiente, muestra independientes) 
+# una muestra ya esta fija, mismo grupo en diferente tiempo, muestra a individuos diferentes
+
+# estadística paramétrica (robustas y parámetros)
+# campana de Gauss, "distribución normal", estándar, prueba de (kolmogorov-smirnof, shapiro-wilkins). "homogeniedad de varianza"
+# ditribución normal, variancia diferente. transformacion de datos
+
+# estadistica no paramétrica (no tan sensibles)
+
+# alpha=0.05 (95%). límite para decidir entre las pruebas que se comparan
+# P-valve (0-1). Ayuda a alpha. diferencias
+# PREGUNTA -> H0=nula (no existen diferencias), H1=alternativa (hay diferencias).
+
+# subset, boxplot, prueba dt "t.test(Sp.FC$Altura ~ Sp.FC$Especie) 
+
