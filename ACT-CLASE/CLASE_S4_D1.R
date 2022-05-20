cd1066ecbe019dd498ac7961a3d8a2cf751b8685
santiago <santiago_forestal@hotmail.com>
2022-05-20 14:07:23 -0500

    #ACTUALIZADO #SANTIAGO #09/02/2022

---
+++ ACT-CLASE/CLASE_S4_D1.R
@@ -0,0 +1,31 @@
+ # MAG
+ # 09/02/2022
+ # Sesion semana 4 sala de computo
+
+setwd("C:/Users/Usuario/Documents/SANTIAGO/NUEVO_ESTADISTICA.SANTIAGO2022/ACT-CLASE")
+
+# Importar datos ----------------------------------------------------------
+
+ vivero <- read.csv("BD_vivero.csv" , header = TRUE)
+
+ # Medidas de tendencia central
+ 
+  mean(vivero$IE) 
+  median(vivero$IE)
+  range(vivero$IE)
+  fivenum(vivero$IE)
+  
+  boxplot(vivero$IE)
+  hist(vivero$IE, ylim = c(0,12))  
+
+  
+  table(vivero$IE)
+  ftable(vivero$IE) 
+  
+  frec <- table(vivero$IE)
+ 
+
+
+
+
+
